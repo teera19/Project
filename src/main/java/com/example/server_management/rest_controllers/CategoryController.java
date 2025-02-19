52e138e0f18e8ca6b624d2875a0aa9eb8209031b
teera <teera.so@rmuti.ac.th>
2025-02-20 00:56:09 +0700

    category

--- src/main/java/com/example/server_management/rest_controllers/CategoryController.java
+++ src/main/java/com/example/server_management/rest_controllers/CategoryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/categorie")
 public class CategoryController {
 
     @Autowired

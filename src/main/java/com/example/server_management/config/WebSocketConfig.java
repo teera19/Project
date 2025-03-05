9e4a6f977c5ee304afe82c9ac0580d433281efc3
teera <teera.so@rmuti.ac.th>
2025-03-05 07:24:14 +0700

    chat

--- src/main/java/com/example/server_management/config/WebSocketConfig.java
+++ src/main/java/com/example/server_management/config/WebSocketConfig.java
@@ -11,16 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        // เปิดการใช้งาน message broker สำหรับการส่งข้อความไปที่ "/topic"
-        config.enableSimpleBroker("/topic");
-        // ใช้ prefix "/app" สำหรับแอปพลิเคชันที่ส่งข้อความ
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // เพิ่ม endpoint สำหรับการเชื่อมต่อ
-        registry.addEndpoint("/chat").withSockJS();  // SockJS ใช้ fallback เมื่อ WebSocket ไม่สามารถใช้งานได้
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("*"); // ✅ อนุญาตทุกโดเมน
     }
 }

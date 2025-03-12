87e4bcff0270d0ad11ccec67f218265d4e31ade7
teera <teera.so@rmuti.ac.th>
2025-03-12 20:20:04 +0700

    delete product

--- src/main/java/com/example/server_management/rest_controllers/DeleteProduct.java
+++ src/main/java/com/example/server_management/rest_controllers/DeleteProduct.java
@@ -5,7 +5,6 @@ import com.example.server_management.models.Product;
 import com.example.server_management.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
